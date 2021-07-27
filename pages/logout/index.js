4fb4bf84d09df95e4c729f56f59a6a634f3c3e43
jpmariano <juanpaolo.mariano28@gmail.com>
2021-07-27 11:51:43 +0800

    Added a Swal component in Logout page

--- pages/logout/index.js
+++ pages/logout/index.js
@@ -1,13 +1,31 @@
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
+import Swal from 'sweetalert2';
 
 function Logout() {
 	const router = useRouter();
 	
 	// To clear user's credentials
 	useEffect(() => {
-		localStorage.clear();
-		router.push('/');
+		async function logOutUser() {
+			try {
+				localStorage.clear();
+				
+				Swal.fire({
+					icon: 'success',
+					title: 'Successfully logged out.',
+					showConfirmButton: false,
+					timer: 1234
+				});
+				
+				await router.push('/');
+				
+			} catch (err) {
+				console.error(err);
+			}
+		}
+		
+		logOutUser();
 	});
 	
 	return null;
