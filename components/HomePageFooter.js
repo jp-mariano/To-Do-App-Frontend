b7402876579002c9b7e891bfaaec25bce4df6649
jpmariano <juanpaolo.mariano28@gmail.com>
2021-08-04 20:48:56 +0800

    Refactor Home Page Footer component

--- components/HomePageFooter.js
+++ components/HomePageFooter.js
@@ -1,8 +1,10 @@
+import Image from 'next/image';
+
 function HPFooter() {
 	return (
 		<div id='footer-component'>
-			<h5 id='my-name' className='text-center'>
-				Made with <img src='/pixel-heart.png' height={ 28 } width={ 28 } alt='love' /> by <a href='https://jpmariano.vercel.app/' target='_blank'>Paolo Mariano</a>.
+			<h5 id='footer-text' className='text-center'>
+				Made with <Image id='pixel-heart-img' src='/pixel-heart.png' height={ 19 } width={ 19 } alt='love' /> by <a href='https://jpmariano.vercel.app/' target='_blank' rel='noreferrer'>Paolo Mariano</a>.
 			</h5>
 		</div>
 	);
